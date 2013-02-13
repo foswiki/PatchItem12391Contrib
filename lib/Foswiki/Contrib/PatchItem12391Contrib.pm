8914d7afc05fff63a7eb0a9da1adb039b3193070
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-02-13 17:30:51 +0000

    Item12391: Patch contrib for Item12391
    git-svn-id: http://svn.foswiki.org/trunk/PatchItem12391Contrib@16536 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Contrib/PatchItem12391Contrib.pm
@@ -0,0 +1,44 @@
+# See bottom of file for default license and copyright information
+
+=begin TML
+
+---+ package Foswiki::Contrib::PatchItem12391Contrib
+
+This is a stub module for a new contrib. Customise this module as
+required. 
+
+=cut
+
+package Foswiki::Contrib::PatchItem12391Contrib;
+
+# Always use strict to enforce variable scoping
+use strict;
+use warnings;
+
+our $VERSION = '1.0';
+our $RELEASE = '1.0';
+
+our $SHORTDESCRIPTION = 'Patch a critical vulnerability in Locale::Maketext.';
+
+1;
+
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Author: GeorgeClark
+
+Copyright (C) 2008-2011 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
